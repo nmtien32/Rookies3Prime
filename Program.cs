4230899f4f9053237e504a6591c4bf6dda278eae
nmtien32 <67999038+nmtien32@users.noreply.github.com>
2022-10-07 00:21:27 +0700

    submit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Rookies3Prime
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GetPrime(0, 50);
+            GetPrime(51, 100);
+            Console.ReadKey();
+        }
+        static async Task GetPrime(int min, int max)
+        {
+            await Task.Run(() =>
+            {
+                for (int i = min; i <= max; i++)
+                {
+                    if (IsPrime(i))
+                    {
+                        Console.Write(" " + i);
+                    }
+                }
+            });
+        }
+        static bool IsPrime(int a)
+        {
+            bool isPrime = true;
+            if (a < 2)
+            {
+                isPrime = false;
+                return isPrime;
+            }
+            else
+            {
+                for (int i = 2; i < a; i++)
+                {
+                    if (a % i == 0)
+                    {
+                        isPrime = false;
+                        return isPrime;
+                    }
+                }
+                isPrime = true;
+                return isPrime;
+            }
+        }
+    }
+}
